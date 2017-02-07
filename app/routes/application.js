4f12609f74ebd580831cc9639ca6e44da61c8df9
Miles Zimmerman <miles@maztek.io>
2017-02-07 09:02:38 -0800

    Adding document title plugin

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,10 @@ export default Ember.Route.extend({
     return this.get('session').fetch().catch(() => {});
   },
 
+  title(tokens) {
+    return (tokens.length ? tokens.reverse().join(' - ') + ' - ' : '') + 'byome';
+  },
+
   actions: {
     accessDenied() {
       this.transitionTo('login');
