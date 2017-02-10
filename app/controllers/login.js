ed3fcea45bd849aa4723d7f1b060db90273d010d
Miles Zimmerman <miles@maztek.io>
2017-02-09 21:33:59 -0800

    Adding more interacting login with errors

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,8 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
+  loggingIn: false,
+  errorMessage: null,
+
   actions: {
     signIn(user) {
+      this.set('loggingIn', true);
       const { email, password } = user.getProperties('email', 'password');
       this.get('session').open('firebase', {
         provider: 'password',
@@ -12,11 +16,26 @@ export default Ember.Controller.extend({
         password: password
       }).then(() => {
         this.transitionToRoute('dashboard');
-      });
+      }).catch(this.handleLoginErrors.bind(this));
     },
 
     resetPassword(user) {
       console.log(user.get('email'));
+    }
+  },
+
+  handleLoginErrors(error) {
+    this.set('loggingIn', false);
+    if (error.code === "auth/user-not-found") {
+      this.set('errorMessage', 'Email not found. Please check spelling.');
+    } else
+    if (error.code === "auth/wrong-password") {
+      this.set('errorMessage', 'The given password is incorrect.');
+    } else
+    if (error.code === "auth/invalid-email") {
+      this.set('errorMessage', error.message);
+    } else {
+      this.set('errorMessage', 'Something went wrong. Please try again.');
     }
   }
 });
