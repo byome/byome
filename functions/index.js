aef0fa0147d6d3c976f1b62bf84709c40b1c9f0c
Miles Zimmerman <miles@maztek.io>
2017-03-14 15:22:28 -0700

    Testing function removal

--- functions/index.js
+++ functions/index.js
@@ -16,50 +16,55 @@ const serversRef = functions.database.ref('servers');
 
 
 // User Session Events
-exports.playerConnected = functions.https.onRequest((req, res) => {
-  // Fail if invalid HTTP request
-  if (!req.method === 'POST') {
-    res.status(403).send('Forbidden!');
-  }
-
-  // Get Data
-  const data = req.body;
-
-  // Fail if invalid API key
-  if (!ServerKeys[data.apiKey]) {
-    res.status(403).send('Forbidden!');
-  }
-
-  playersRef
-    .child(data.id)
-    .once('value')
-    .then((player) => {
-      if (!player.exists()) {
-        playersRef.child(data.id).set({ name: data.name });
-        return playersRef.child(data.id);
-      } else {
-        return player.ref;
-      }
-    })
-    .then((player) => {
-      player.child(`servers/${data.server}`).set(true);
-      serversRef.child(data.server).child(`players/${data.id}`).set(true);
-      return player;
-    })
-    .then((player) => {
-      connectionsRef.child(`${data.server}-${data.id}`).set({
-        player: data.id,
-        server: data.server,
-        ipAddress: data.ipAddress,
-        connected: true
-      });
-      serversRef.child(data.server).child(`connections/${data.server}-${data.id}`).set(true);
-      player.child('connections').child(`${data.server}-${data.id}`).set(true);
-    })
-    .then(() => {
-      res.status(200).send('OK');
-    });
-});
+// exports.playerConnected = functions.https.onRequest((req, res) => {
+//   console.log("Request received");
+//   console.log(req.body);
+//
+//   // Fail if invalid HTTP request
+//   if (!req.method === 'POST') {
+//     res.status(403).send('Forbidden!');
+//   }
+//
+//   // Get Data
+//   const data = req.body;
+//
+//   // Fail if invalid API key
+//   if (!ServerKeys[data.apiKey]) {
+//     res.status(403).send('Forbidden!');
+//   }
+//
+//   res.status(200).send("OK");
+//   //
+//   // playersRef
+//   //   .child(data.id)
+//   //   .once('value')
+//   //   .then((player) => {
+//   //     if (!player.exists()) {
+//   //       playersRef.child(data.id).set({ name: data.name });
+//   //       return playersRef.child(data.id);
+//   //     } else {
+//   //       return player.ref;
+//   //     }
+//   //   })
+//   //   .then((player) => {
+//   //     player.child(`servers/${data.server}`).set(true);
+//   //     serversRef.child(data.server).child(`players/${data.id}`).set(true);
+//   //     return player;
+//   //   })
+//   //   .then((player) => {
+//   //     connectionsRef.child(`${data.server}-${data.id}`).set({
+//   //       player: data.id,
+//   //       server: data.server,
+//   //       ipAddress: data.ipAddress,
+//   //       connected: true
+//   //     });
+//   //     serversRef.child(data.server).child(`connections/${data.server}-${data.id}`).set(true);
+//   //     player.child('connections').child(`${data.server}-${data.id}`).set(true);
+//   //   })
+//   //   .then(() => {
+//   //     res.status(200).send('OK');
+//   //   });
+// });
 
 
 // exports.playerConnected = functions.database.ref('/events/player_connected').onWrite(event => {
