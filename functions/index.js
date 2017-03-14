020b7337000572ff29b59b6ae95014624393571b
Miles Zimmerman <miles@maztek.io>
2017-03-14 15:27:33 -0700

    Updatingg playerConnected function to do its job

--- functions/index.js
+++ functions/index.js
@@ -17,9 +17,6 @@ const serversRef = functions.database.ref('servers');
 
 // User Session Events
 exports.playerConnected = functions.https.onRequest((req, res) => {
-  console.log("Request received");
-  console.log(req.body);
-
   // Fail if invalid HTTP request
   if (!req.method === 'POST') {
     res.status(403).send('Forbidden!');
@@ -33,37 +30,38 @@ exports.playerConnected = functions.https.onRequest((req, res) => {
     res.status(403).send('Forbidden!');
   }
 
-  res.status(200).send("OK");
-  //
-  // playersRef
-  //   .child(data.id)
-  //   .once('value')
-  //   .then((player) => {
-  //     if (!player.exists()) {
-  //       playersRef.child(data.id).set({ name: data.name });
-  //       return playersRef.child(data.id);
-  //     } else {
-  //       return player.ref;
-  //     }
-  //   })
-  //   .then((player) => {
-  //     player.child(`servers/${data.server}`).set(true);
-  //     serversRef.child(data.server).child(`players/${data.id}`).set(true);
-  //     return player;
-  //   })
-  //   .then((player) => {
-  //     connectionsRef.child(`${data.server}-${data.id}`).set({
-  //       player: data.id,
-  //       server: data.server,
-  //       ipAddress: data.ipAddress,
-  //       connected: true
-  //     });
-  //     serversRef.child(data.server).child(`connections/${data.server}-${data.id}`).set(true);
-  //     player.child('connections').child(`${data.server}-${data.id}`).set(true);
-  //   })
-  //   .then(() => {
-  //     res.status(200).send('OK');
-  //   });
+  playersRef
+    .child(data.id)
+    .once('value')
+    .then((player) => {
+      if (!player.exists()) {
+        playersRef.child(data.id).set({ name: data.name });
+        return playersRef.child(data.id);
+      } else {
+        return player.ref;
+      }
+    })
+    .then((player) => {
+      player.child(`servers/${data.server}`).set(true);
+      serversRef.child(data.server).child(`players/${data.id}`).set(true);
+      return player;
+    })
+    .then((player) => {
+      connectionsRef.child(`${data.server}-${data.id}`).set({
+        player: data.id,
+        server: data.server,
+        ipAddress: data.ipAddress,
+        connected: true
+      });
+      serversRef.child(data.server).child(`connections/${data.server}-${data.id}`).set(true);
+      player.child('connections').child(`${data.server}-${data.id}`).set(true);
+    })
+    .then(() => {
+      res.status(200).send('OK');
+    })
+    .catch((error) => {
+      res.status(500).send("There was an error.");
+    });
 });
 
 
