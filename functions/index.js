1632493e60f7779345956303fba1de28ce8fb836
Miles Zimmerman <miles@maztek.io>
2017-03-14 15:48:22 -0700

    Finishing playerConnected and playerDisconnected functions

--- functions/index.js
+++ functions/index.js
@@ -20,6 +20,7 @@ exports.playerConnected = functions.https.onRequest((req, res) => {
   // Fail if invalid HTTP request
   if (!req.method === 'POST') {
     res.status(403).send('Forbidden!');
+    return;
   }
 
   // Get Data
@@ -28,6 +29,7 @@ exports.playerConnected = functions.https.onRequest((req, res) => {
   // Fail if invalid API key
   if (!ServerKeys[data.apiKey]) {
     res.status(403).send('Forbidden!');
+    return;
   }
 
   playersRef
@@ -65,45 +67,29 @@ exports.playerConnected = functions.https.onRequest((req, res) => {
 });
 
 
-// exports.playerConnected = functions.database.ref('/events/player_connected').onWrite(event => {
-//   const data = event.data.val();
-//   return playersRef
-//     .child(data.id)
-//     .once('value')
-//     .then((player) => {
-//       if (!player.exists()) {
-//         playersRef.child(data.id).set({ name: data.name });
-//         return playersRef.child(data.id);
-//       } else {
-//         return player.ref;
-//       }
-//     })
-//     .then((player) => {
-//       player.child(`servers/${data.server}`).set(true);
-//       serversRef.child(data.server).child(`players/${data.id}`).set(true);
-//       return player;
-//     })
-//     .then((player) => {
-//       connectionsRef.child(`${data.server}-${data.id}`).set({
-//         player: data.id,
-//         server: data.server,
-//         ipAddress: data.ipAddress,
-//         connected: true
-//       });
-//       serversRef.child(data.server).child(`connections/${data.server}-${data.id}`).set(true);
-//       player.child('connections').child(`${data.server}-${data.id}`).set(true);
-//     })
-//     .then(() => {
-//       return resolve();
-//     });
-// });
+exports.playerDisconnected = functions.https.onRequest((req, res) => {
+  // Fail if invalid HTTP request
+  if (!req.method === 'POST') {
+    res.status(403).send('Forbidden!');
+    return;
+  }
 
-// exports.playerDisconnected = functions.database.ref('/events/player_disconnected').onWrite(event => {1
-//   const data = event.data.val();
-//   connectionsRef.child(`${data.server}-${data.id}`).update({
-//     connected: false
-//   })
-//   .then(() => {
-//     resolve();
-//   });
-// });
+  // Get Data
+  const data = req.body;
+
+  // Fail if invalid API key
+  if (!ServerKeys[data.apiKey]) {
+    res.status(403).send('Forbidden!');
+    return;
+  }
+
+  connectionsRef.child(`${data.server}-${data.id}`).update({
+    connected: false
+  })
+  .then(() => {
+    res.status(200).send("OK");
+  })
+  .catch((error) => {
+    res.status(500).send("Something went wrong.");
+  });
+});
