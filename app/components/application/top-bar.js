c5e5b825d88b0fa44c5fadfc64a93f11efa3d550
Miles Zimmerman <miles@byome.io>
2017-04-29 03:16:15 -0700

    Fuck it i'll do it myself

--- app/components/application/top-bar.js
+++ app/components/application/top-bar.js
@@ -2,14 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
-  
+
   tagName: 'nav',
   classNames: [
     'navbar',
     'navbar-full',
     'navbar-inverse',
     'navbar-fixed-top',
-    'mai-top-header'
+    'mai-top-header',
+    'mai-top-header-show-logo'
   ],
 
   didInsertElement() {
