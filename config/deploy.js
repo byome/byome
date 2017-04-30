1e41efa4d48b1aaa335b8a855b4643a522ef9174
Miles Zimmerman <miles@byome.io>
2017-04-30 09:24:49 -0700

    Updated deploy to use token

--- config/deploy.js
+++ config/deploy.js
@@ -4,32 +4,29 @@ module.exports = function(deployTarget) {
   var ENV = {
     build: {},
     sentry: {
-      publicUrl: 'https://byome.io', // the URL or CDN your js assets are served from
-      sentryUrl: 'https://sentry.io', // the sentry install you're using, https://sentry.io for hosted accounts
-      sentryOrganizationSlug: 'byome',
-      sentryProjectSlug: 'byome-frontend',
-      sentryApiKey: '50400eb4ed394fecb2b027abeccdbea19c3c78198bb04011bee3997434b4ba54',
-      sentryBearerApiKey: '50400eb4ed394fecb2b027abeccdbea19c3c78198bb04011bee3997434b4ba54'
+      publicUrl: process.env.SENTRY_PUBLIC_URL, // the URL or CDN your js assets are served from
+      sentryUrl: process.env.SENTRY_URL, // the sentry install you're using, https://sentry.io for hosted accounts
+      sentryOrganizationSlug: process.env.SENTRY_ORGANIZATION_SLUG,
+      sentryProjectSlug: process.env.SENTRY_PROJECT_SLUG,
+      sentryApiKey: process.env.SENTRY_API_KEY,
+      sentryBearerApiKey: process.env.SENTRY_BEARER_API_KEY
+    },
+    firebase: {
+      deployToken: process.env.FIREBASE_DEPLOY_TOKEN
     }
   };
 
   if (deployTarget === 'development') {
     ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
   }
 
   if (deployTarget === 'staging') {
     ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
   }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
   }
 
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
   return ENV;
 };
